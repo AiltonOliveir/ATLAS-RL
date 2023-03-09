1d2bc5a6e48f17370712b7270ec3d673113e4f57
CaioBBrasil <119973096+CaioBBrasil@users.noreply.github.com>
2023-03-09 11:12:13 -0300

    Add files via upload
    Code to automatically get pictures in airsim

---
+++ gettingPicturesAirsim.py
@@ -0,0 +1,114 @@
+import airsim
+import os
+import tempfile
+import numpy as np
+import random as rd
+
+#Correction of a bug that input twice
+def cBug1():
+    airsim.wait_key()
+
+#Defining the name and type of camera
+CAM_NAME = "fixed1"
+IS_EXTERNAL_CAM = True
+
+#start some global variables
+typePose= 0
+interaction = 0
+
+#starting the conection with airsim
+client = airsim.VehicleClient()
+client.confirmConnection()
+
+#Making the path to store the pictures
+tmp_dir = os.path.join(tempfile.gettempdir(), "airsim_cv_mode")
+print ("Saving images t %s" % tmp_dir)
+try:
+    os.makedirs(tmp_dir)
+except OSError:
+    if not os.path.isdir(tmp_dir):
+        raise
+
+#starting the simulation
+airsim.wait_key('Press any key to get images')
+cBug1()
+
+#defining what type of image we want
+requests = [airsim.ImageRequest(CAM_NAME, airsim.ImageType.Scene)]
+
+#starting takng pictures
+while interaction < 100:
+    
+    #getting the pose of the car and spliting in position and orientation
+    objectPose = client.simGetObjectPose("carro")
+    objectPosition = objectPose.position
+    objectOrientation = objectPose.orientation
+
+    #correction factor of car's z_val
+    objectZajust = objectPosition.z_val - 1.012
+
+    #Puting the camera close to the car
+    x = -13
+    y = 2
+    
+    # x = objectPosition.x_val + 3
+    # y = objectPosition.y_val 
+
+    #Defining the camera's position
+    a = 2 + (2*rd.random())
+    positions = [(x, y, 0), (x, y + a, 0), (x-a, y+6, 0), (x -6 -a, y+6, 0), (x - 12, y + a, 0), (x - 12, y - a, 0), (x -6 -a, y-6, 0), (x, y-a, 0),
+                 (x, y, -a), (x, y + a, -a), (x-a, y+6, -a), (x -6 -a, y+6, -a), (x - 12, y + a, -a), (x - 12, y - a, -a), (x -6 -a, y-6, -a), (x, y-a, -a)]
+
+    filename = os.path.join(tmp_dir,'image' + "_" + str(interaction))
+    
+    #getting the relative distance between camera and car
+    x_rel = positions[typePose][0]-objectPosition.x_val
+    y_rel = positions[typePose][1]-objectPosition.y_val
+    z_rel = positions[typePose][2]-(objectZajust)
+
+    #Adjusting the camera pitch if it is higher than the car
+    if z_rel < -2:
+        pitch = -np.arctan(-z_rel/np.sqrt((x_rel**2)+(y_rel)**2))
+        
+    else:
+         pitch = 0
+
+    #Adjusting the camera yaw according to quadrant     
+    if x_rel > 0 and y_rel > 0:
+        yaw = np.arctan((y_rel)/(x_rel))
+        yaw = np.pi + yaw
+
+    elif x_rel < 0 and y_rel > 0:
+        yaw = np.arctan((y_rel)/(-x_rel))
+        yaw = -yaw
+    
+    elif x_rel < 0 and y_rel < 0:
+        yaw = np.arctan((-y_rel)/(-x_rel))
+
+    elif x_rel > 0 and y_rel < 0:
+         yaw = np.arctan((-y_rel)/(x_rel))
+         yaw = np.pi-yaw
+
+    else:
+         yaw = 0
+
+    #setting the new camera's pose
+    npose = airsim.Pose(position_val=airsim.Vector3r(positions[typePose][0], positions[typePose][1], positions[typePose][2] ), orientation_val=airsim.to_quaternion(pitch, 0, yaw ))
+    client.simSetCameraPose(camera_name=CAM_NAME, pose=npose, external=IS_EXTERNAL_CAM)
+    
+    #getting the pictures
+    responses = client.simGetImages(requests, external=IS_EXTERNAL_CAM)
+    for i, response in enumerate(responses):
+        if response.pixels_as_float:
+                airsim.write_pfm(os.path.normpath(filename + '.pfm'), airsim.get_pfm_array(response))
+        else:
+                airsim.write_file(os.path.normpath(filename + '.png'), response.image_data_uint8)
+    
+    #adjust to the next type of camera's position
+    typePose = typePose+1
+    if typePose >= 16:
+         typePose = 0
+    
+    interaction = interaction + 1
+
+#Angulos estao em radiano
