871513ed12eee63349dfaeaac92fcd4fe942355e
Carnot Braun <37417600+carnotbraun@users.noreply.github.com>
2023-05-15 23:01:01 -0300

    Update uav_client.py

--- envs/uav_client.py
+++ envs/uav_client.py
@@ -3,25 +3,27 @@ import airsim
 
 class UAV():
     def __init__(self):
+        self.uav_id = 'Drone1'
         print('Connecting Airsim')
         self.client = airsim.MultirotorClient()
         self.client.confirmConnection()
         print('Airsim already')
         self.image_request = airsim.ImageRequest(0, airsim.ImageType.Scene) #png
         self.encode_action = {
-            (0,0,1): 0,(0,0,-1): 1,
-            (1,0,0): 2, (1,1,0): 3,
-            (0,1,0): 4, (-1,1,0):5,
-            (-1,0,0):6,(-1,-1,0):7,
-            (0,-1,0):8, (1,-1,0): 9
-            }
-    
-    def _start_fligth(self,uav_id="uav1"):
-        self.client.enableApiControl(True, uav_id)
-        self.client.armDisarm(True, uav_id)
-        self.client.takeoffAsync(vehicle_name=uav_id).join()
+            0 : (0,0,1), 1 : (0,0,-1),
+            2 : (1,0,0), 3 : (1,1,0),
+            4 : (0,1,0), 5 : (-1,1,0),
+            6 : (-1,0,0),7 : (-1,-1,0),
+            8 : (0,-1,0), 9 : (1,-1,0)
+        }
 
-    def _gps_get(self,uav_id="uav1"):
+    
+    def _start_fligth(self):
+        self.client.enableApiControl(True, self.uav_id)
+        self.client.armDisarm(True, self.uav_id)
+        self.client.takeoffAsync(vehicle_name=self.uav_id).join()
+
+    def _gps_get(self):
         """
         The postition inside the returned MultiRotorState is in the frame of the vehicle's starting point
         
@@ -40,26 +42,26 @@ class UAV():
         timestamp=0
 
         """
-        x = self.client.getMultirotorState(vehicle_name=uav_id)
+        x = self.client.getMultirotorState(vehicle_name=self.uav_id)
         return x
 
     def _reset_flight(self):
         self.client.reset()
         self._start_fligth()    
     
-    def airsim_land(self,uav_id="uav1"):
-        landed = self.client.getMultirotorState(vehicle_name=uav_id).landed_state
+    def airsim_land(self):
+        landed = self.client.getMultirotorState(vehicle_name=self.uav_id).landed_state
         #Ailton OBS: Check if this if works
         if landed == airsim.LandedState.Landed:
             print("already landed...")
             return None
-        self.client.armDisarm(False, uav_id)
+        self.client.armDisarm(False, self.uav_id)
 
-    def discrete_action(self,agent_action,speed = 5,uav_id="uav1"):
+    def discrete_action(self,agent_action,speed = 5):
         x,y,z = self.encode_action[agent_action]
         self.client.enableApiControl(True)
         self.client.moveToPositionAsync(x, y, z, speed, 1e+38, airsim.DrivetrainType.ForwardOnly,
-                                         airsim.YawMode(False,0), vehicle_name=uav_id)
+                                         airsim.YawMode(False,0), vehicle_name=self.uav_id)
 
     def _positions(self):
         pass
